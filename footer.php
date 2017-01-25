2a43466164dc14f06d1b158ef77f9ed5e49c4132
Severak <strajt9@seznam.cz>
2017-01-25 15:15:02 +0100

    fix: Zoom taky v tematku.

--- footer.php
+++ footer.php
@@ -3,5 +3,9 @@
 </div>
 </div>
 
+<!--[if !IE]>-->
+<script src="<?php echo theme_url('/js/medium-zoom.min.js'); ?>"></script>
+<script>mediumZoom('.flogiston-article > img, .flogiston-article p > img, .flogiston-article .figure > img');</script>
+<!--<![endif]-->
 </body>
 </html>
