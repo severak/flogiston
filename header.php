2cf5a519e4924f2055a8af2a1b41616f34c0fdaf
Severak <strajt9@seznam.cz>
2017-01-25 15:10:52 +0100

    fix: css ma byt minifikovano

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title><?php echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php echo theme_url('/css/flogiston.css'); ?>">
+	<link rel="stylesheet" href="<?php echo theme_url('/css/flogiston.min.css'); ?>">
 	<meta property="og:title" content="<?php echo site_name(); ?>" />
 	<meta property="og:image" content="<?php echo theme_url('/css/rozvedcik.jpg'); ?>" />
 	<meta property="og:type" content="website" />
