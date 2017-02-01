81e9ddea8e0cc77826a9eaacb1bb74dc070a1a84
Severak <strajt9@seznam.cz>
2017-02-01 23:20:43 +0100

    dva fixy pro blog

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="<?php echo theme_url('/css/flogiston.min.css'); ?>">
 	<meta property="og:title" content="<?php echo site_name(); ?>" />
-	<meta property="og:image" content="<?php echo theme_url('/css/rozvedcik.jpg'); ?>" />
+	<meta property="og:image" content="<?php echo theme_url('/css/background-02.jpg'); ?>" />
 	<meta property="og:type" content="website" />
 	<meta property="og:description" content="<?php echo site_description(); ?>" />
 	<!--[if IE]>
