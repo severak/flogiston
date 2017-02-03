6c9f9f9dc68e3e4239d4d6329308c33901b1f7bd
Severak <strajt9@seznam.cz>
2017-02-03 16:47:40 +0100

    fix: okraje 404ky

--- 404.php
+++ 404.php
@@ -1,9 +1,11 @@
 <?php theme_include('header'); ?>
 
-	<section class="content wrap">
+<div class="flogiston-article">
+
 		<h1>Nenalezeno</h1>
 
 		<p>Stránka nenalezena, prosím, vraťte se na <a href="<?php echo base_url(); ?>">začátek</a>.</p>
-	</section>
+
+</div>
 
 <?php theme_include('footer'); ?>
